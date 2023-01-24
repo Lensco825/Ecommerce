6b7d1a378728e305ac68a49f9d84010b45c62390
Lens <Lensdorsainvil711@outlook.com>
2023-01-24 18:03:38 -0500

    Added Shoe section

--- shop.js
+++ shop.js
@@ -4,4 +4,10 @@ var item_Width = items[0].getBoundingClientRect().width;
 var nextButton = document.querySelector('.button-forward');
 var backButton = document.querySelector('.button-back');
 
+
+items.forEach((card, index) => {
+    card.style.left = cardSize * index + 'px';
+  })
+
+
 console.log(item_Width);
