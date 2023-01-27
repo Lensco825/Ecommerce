e60a873970dff25d5351a88bd162d51ff310e74c
Lens <Lensdorsainvil711@outlook.com>
2023-01-27 17:18:18 -0500

    Added Mens Section

--- shop.js
+++ shop.js
@@ -1,18 +1,15 @@
 var itemContainer = document.querySelector('.item_container')
 var items = Array.from(itemContainer.children);
 var item_Width = items[0].getBoundingClientRect().width; 
-var nextButton = document.querySelector('.button-forward');
-var backButton = document.querySelector('.button-back');
+var nextButton = document.querySelectorALL('.button-forward');
+var backButton = document.querySelectorALL('.button-back');
 
 
-
-console.log(item_Width);
-
-
-
-nextButton.addEventListener('click', function() {
-itemContainer.style.transform = 'translateX(-20%)';
-});
+nextButton.forEach(nextButton => {
+    nextButton.addEventListener('click', function() {
+        itemContainer.style.transform = 'translateX(-20%)';
+        });        
+  });
 
 
 backButton.addEventListener('click', function() {
