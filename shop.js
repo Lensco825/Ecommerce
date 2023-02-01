ef2a2ffbf61520cdfee1fa7a2532dd32f2ae80d0
Lens <Lensdorsainvil711@outlook.com>
2023-01-31 19:01:10 -0500

    Fixed carousels and added footer.

--- shop.js
+++ shop.js
@@ -1,17 +1,24 @@
 var itemContainer = document.querySelector('.item_container')
 var items = Array.from(itemContainer.children);
-var item_Width = items[0].getBoundingClientRect().width; 
-var nextButton = document.querySelectorALL('.button-forward');
-var backButton = document.querySelectorALL('.button-back');
+var item_Width = items[0].getBoundingClientRect().width;
+var nextButton = document.querySelectorAll('.button-forward');
+var backButton = document.querySelectorAll('.button-back');
 
+console.log(document.querySelector('html').getBoundingClientRect().width);
 
-nextButton.forEach(nextButton => {
-    nextButton.addEventListener('click', function() {
-        itemContainer.style.transform = 'translateX(-20%)';
-        });        
+// get all carousels in the page
+const carousels = document.body.querySelectorAll(".carousel");
+
+// scan each carousel and assign events to children
+carousels.forEach((carousel) => {
+  // get item_container
+  const itemContainer = carousel.querySelector(".item_container");
+  // assign event to button-back
+  carousel.querySelector(".button-back").addEventListener('click', () => {
+    itemContainer.style.transform = 'translateX(10%)';
   });
-
-
-backButton.addEventListener('click', function() {
-itemContainer.style.transform = 'translateX(20%)';
+  // assign event to button-forward
+  carousel.querySelector(".button-forward").addEventListener('click', () => {
+    itemContainer.style.transform = 'translateX(-40%)';
+  });
 });
